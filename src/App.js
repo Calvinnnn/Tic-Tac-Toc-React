359b17fd86b7175d63dee8f32aef338b98a1b5c2
Omar Emad <oemad3366@gmail.com>
2026-01-07 17:16:39 +0200

    Drawing game board

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="Board">
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
